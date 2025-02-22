8cfe2b36ec67c8cbbc86a62f0e008be4e6806db0
Yesa <29364738+ysyesa@users.noreply.github.com>
2025-02-22 15:46:46 +0800

    Add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This file is the webserver based on Flask to serve the SimpleResume web application.
 """
 from flask import Flask, render_template, make_response
+from flask_wtf.csrf import CSRFProtect
 from users import load_dummy_users
 
 
@@ -9,8 +10,10 @@ APP                 = Flask(__name__)
 APP.config["DEBUG"] = True
 HOST                = "0.0.0.0"
 PORT                = 5000
-DATABASE_USER       = "admin"
-DATABASE_PASSWORD   = "admin12345678"
+
+# CSRF
+csrf = CSRFProtect()
+csrf.init_app(APP)
 
 
 @APP.route("/")
